fc2746c83b2bd5af33a1cb90aa1542ebe6ec9aad
akrupsky <antarh@gmail.com>
2021-05-27 23:16:11 +0300

    The first example was added

---
+++ Medium.Encapsulation/BasicEncapsulation/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium.Encapsulation.BasicEncapsulation
+{
+    public class Car
+    {
+        private string _color;
+
+        public Car(string color)
+        {
+            _color = color;
+        }
+
+        public void Repaint(string color)
+        {
+            _color = color;
+        }
+    }
+}
---
+++ Medium.Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Medium.Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
