a2f6b3892bad3f1c0317804adc2bfd8f566e995b
akrupsky <antarh@gmail.com>
2021-05-27 23:34:27 +0300

    the second example was added

---
+++ Medium.Encapsulation/PropertyDeclaration/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium.Encapsulation.PropertyDeclaration
+{
+    public class Car
+    {
+        public string Color { get; set; }
+    }
+}
