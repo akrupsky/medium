cfe8a70ab02c865d9fa189d16dd863ad08da2663
akrupsky <antarh@gmail.com>
2021-05-28 09:23:36 +0300

    new examples were added

---
+++ Medium.Encapsulation/OldFashionStyle/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium.Encapsulation.OldFashionStyle
+{
+    public class Car
+    {
+        private string _color;
+
+        public string Color
+        {
+            get
+            {
+                // Any logic
+                return _color;
+            }
+            set
+            {
+                // Any logic
+                _color = value;
+                // Any logic
+            }
+        }
+    }
+}
---
+++ Medium.Encapsulation/StateProtection/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium.Encapsulation.StateProtection
+{
+    public class Car
+    {
+        private string _color;
+
+        public Car(string color)
+        {
+            _color = color;
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public void SetColor(string color)
+        {
+            _color = color;
+        }
+    }
+}
